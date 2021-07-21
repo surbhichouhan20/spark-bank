4a6ca5c9a0b98a9e93938fee62922519e018edb8
surbhichouhan20 <76529442+surbhichouhan20@users.noreply.github.com>
2021-07-21 16:47:28 +0530

    Update selecteduserdetail.php

--- selecteduserdetail.php
+++ selecteduserdetail.php
@@ -161,9 +161,9 @@ if(isset($_POST['submit']))
             </div>
         </form>
     </div>
-    <footer class="text-center mt-5 py-2">
-            <p>&copy 2021. Made by <b>Priyanshi Dongre</b> <br></p>
-    </footer>
+    <footer  style="margin-top:11%;">
+        <p style="text-align:center;">&copy all right reserved 2021. Hosted by <b>Surbhi Chouhan</b> <br> </p>
+      </footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
